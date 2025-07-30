f8e78addd2256088f10bc1559ab7311d7a995b12
Khalid <khalidald4z@gmail.com>
2025-07-30 12:03:04 +0300

    Initial Commit

---
+++ Ravlo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ravlo
+//
+//  Created by Khalid R on 05/02/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ravlo/RavloApp.swift
@@ -0,0 +1,17 @@
+//
+//  RavloApp.swift
+//  Ravlo
+//
+//  Created by Khalid R on 05/02/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct RavloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
